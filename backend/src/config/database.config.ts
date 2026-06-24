aeb6ba8e7972d889387f85156569b6a1815dcc49
Azfa <muhammadazfahaqqoni@gmail.com>
2026-06-25 00:31:35 +0700

    feat: ganti database dari MySQL ke PostgreSQL

--- backend/src/config/database.config.ts
+++ backend/src/config/database.config.ts
@@ -1,11 +1,11 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const databaseConfig: TypeOrmModuleOptions = {
-  type: 'mysql',
+  type: 'postgres',
   host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '3306'),
-  username: process.env.DB_USERNAME || 'root',
-  password: process.env.DB_PASSWORD || '',
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'admin',
   database: process.env.DB_DATABASE || 'it_org_management',
 
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
